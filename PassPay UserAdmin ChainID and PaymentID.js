9483d871b500a609919c89a1a37d5d06a109c15d
jan-nt <jas@nortronic.com>
2026-08-18 14:07:13 +0200

    Update PassPay UserAdmin ChainID and PaymentID.js

--- PassPay UserAdmin ChainID and PaymentID.js
+++ PassPay UserAdmin ChainID and PaymentID.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PassPay UserAdmin ChainID, PaymentID & Search Helper
 // @namespace    https://nidushan.com
-// @version      1.4
+// @version      1.5
 // @description  Converts ChainID and PaymentID values into clickable links and adds smart search helpers
 // @author       Jan Sinnadurai
 // @homepageURL  https://nidushan.com
